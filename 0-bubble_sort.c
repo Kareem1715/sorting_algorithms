fa3b780268421ba5fd4c434cbff59f0a9f8a13f9
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-07 15:49:22 +0200

    Task 0 - Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+void mySwap(int *first, int *second);
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: This array have a numbers
+ * @size: This is a size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				mySwap(&array[j], &array[j + 1]);
+				print_array(array, size);
+
+			}
+		}
+	}
+}
+
+/**
+ * mySwap - Swaps the two values
+ * @first: First value
+ * @second: Second value
+ */
+void mySwap(int *first, int *second)
+{
+	int tmp;
+
+	tmp = *first;
+	*first = *second;
+	*second = tmp;
+}
