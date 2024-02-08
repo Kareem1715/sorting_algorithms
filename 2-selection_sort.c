18e6c792225b8fb903caa3a22c47fa02a4ffe4cf
Kareem1715 <Kareem.H1715@gmail.com>
2024-02-08 20:28:35 +0200

    Task 2 - Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,45 @@
+#include "sort.h"
+
+void mySwap(int *first, int *second);
+
+/**
+ * selection_sort - sorts an array of integers
+ * in ascending order
+ * @array: This array have a numbers
+ * @size: This is a size of array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int *smallest;
+
+	for (i = 0; i < size; i++)
+	{
+		/* Points to first element */
+		smallest = &array[i];
+
+		for (j = i; j < size; j++)
+			if (array[j] < *smallest)
+			/* Make the pointer points to the smallest */
+				smallest = &array[j];
+
+		/* After finsh the inner loop swap the first to smallest*/
+		mySwap(&array[i], smallest);
+		print_array(array, size);
+	}
+}
+
+
+/**
+ * mySwap - Swaps the two values
+ * @first: First value
+ * @second: Second value
+ */
+void mySwap(int *first, int *second)
+{
+	int tmp;
+
+	tmp = *first;
+	*first = *second;
+	*second = tmp;
+}
